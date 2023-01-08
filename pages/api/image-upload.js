5cc9f0901f7d8250d0b85acdc169b5fd4b28b982
Mali Matiso <s220651337@mandela.ac.za>
2023-01-09 00:37:04 +0200

    API endpoint to upload images

---
+++ pages/api/image-upload.js
@@ -0,0 +1,54 @@
+import { createClient } from '@supabase/supabase-js';
+import { nanoid } from 'nanoid';
+import { decode } from 'base64-arraybuffer';
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+);
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        let { image } = req.body;
+
+        if (!image) {
+            return res.status(500).json({ message: 'No image provided' });
+        }
+
+        try {
+            const contentType = image.match(/data:(.*);base64/)?.[1];
+            const base64FileData = image.split('base64,')?.[1];
+
+            if (!contentType || !base64FileData) {
+                return res.status(500).json({message: 'Image data not valid'});
+            }
+
+            const fileName = nanoid();
+            const ext = contentType.split('/')[1];
+            const path = `${fileName}.${ext}`;
+
+            const {data, error: uploadError} = await supabase.storage
+                .from(process.env.SUPABASE_BUCKET)
+                .upload(path, decode(base64FileData), {
+                    contentType,
+                    upsert: true,
+                });
+
+            if (uploadError) {
+                return res.status(500).json({message: 'Could not upload image'});
+            }
+
+            const url = `${process.env.SUPABASE_URL.replace('.co', '.in')}/storage/v1/object/public/${data.Key}`;
+            return res.status(200).json({url});
+        } catch (e) {
+            res.status(500).json({ message: 'Something went wrong' });
+        }
+    }
+    else
+    {
+        res.setHeader('Allow', ['POST']);
+        res
+            .status(405)
+            .json({ message: `HTTP method ${req.method} is not supported.` });
+    }
+}

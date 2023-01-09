d948dc7d43208baf6b034302d1c801c6ddb37a6a
Mali Matiso <s220651337@mandela.ac.za>
2023-01-09 23:10:43 +0200

    Function to upload image from form

--- components/ListingForm.js
+++ components/ListingForm.js
@@ -6,6 +6,7 @@ import { toast } from 'react-hot-toast';
 import { Formik, Form } from 'formik';
 import Input from '@/components/Input';
 import ImageUpload from '@/components/ImageUpload';
+import axios from "axios";
 
 const ListingSchema = Yup.object().shape({
   title: Yup.string().trim().required(),
@@ -28,7 +29,21 @@ const ListingForm = ({
   const [imageUrl, setImageUrl] = useState(initialValues?.image ?? '');
 
   const upload = async image => {
-    // TODO: Upload image to remote storage
+    if (!image) return;
+
+    let toastId;
+    try {
+      setDisabled(true);
+      toastId = toast.loading('Uploading...');
+      const { data } = await axios.post('/api/image-upload', { image });
+      setImageUrl(data?.url);
+      toast.success('Successfully uploaded', { id: toastId });
+    } catch (e) {
+      toast.error('Unable to upload', { id: toastId });
+      setImageUrl('');
+    } finally {
+      setDisabled(false);
+    }
   };
 
   const handleOnSubmit = async (values = null) => {

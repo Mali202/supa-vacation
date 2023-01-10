cb16529da3f0bffc9ed4a21ae793d4a978987010
Mali Matiso <s220651337@mandela.ac.za>
2023-01-10 22:39:30 +0200

    Fix: retrieve bucket name from env

--- pages/api/image-upload.js
+++ pages/api/image-upload.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
                 return res.status(500).json({message: 'Could not upload image'});
             }
 
-            const url = `${process.env.SUPABASE_URL.replace('.co', '.in')}/storage/v1/object/public/supavacation/${data.path}`;
+            const url = `${process.env.SUPABASE_URL.replace('.co', '.in')}/storage/v1/object/public/${process.env.SUPABASE_BUCKET}/${data.path}`;
             return res.status(200).json({url});
         } catch (e) {
             res.status(500).json({ message: 'Something went wrong' });

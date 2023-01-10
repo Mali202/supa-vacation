9ae2610c6212720916548a424dd27001b3ce4e3a
Mali Matiso <s220651337@mandela.ac.za>
2023-01-10 15:27:00 +0200

    Install next-auth and wrap pages in session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
+import {Toaster} from 'react-hot-toast';
+import {SessionProvider} from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
-  );
+function MyApp({Component, pageProps: { session, ...pageProps }}) {
+    return (
+        <>
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+                <Toaster/>
+            </SessionProvider>
+        </>
+    );
 }
 
 export default MyApp;

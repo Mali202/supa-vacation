3b1379b606d05f13d09e21ff1ae7c827ebbebcdf
Mali Matiso <s220651337@mandela.ac.za>
2023-01-13 19:41:17 +0200

    Define custom auth pages

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,12 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 export default NextAuth({
+    pages: {
+        signIn: '/',
+        signOut: '/',
+        error: '/',
+        verifyRequest: '/',
+    },
     providers: [
         EmailProvider({
             server: {

36f726ac731b48fce85599e1b9d2a3e2fd7b5cc8
Mali Matiso <s220651337@mandela.ac.za>
2023-01-24 22:30:48 +0200

    getServerSideProps function to fetch homes listed by user

--- pages/homes.js
+++ pages/homes.js
@@ -1,5 +1,32 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+import {PrismaClient} from "@prisma/client";
+import {getSession} from "next-auth/react";
+
+const prisma = new PrismaClient();
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        };
+    }
+
+    const homes = await prisma.home.findMany({
+        where: { owner: { email: session.user.email } },
+        orderBy: { createdAt: 'desc' },
+    });
+
+    return {
+        props: {
+            homes: JSON.parse(JSON.stringify(homes)),
+        },
+    };
+}
 
 const Homes = ({ homes = [] }) => {
     return (

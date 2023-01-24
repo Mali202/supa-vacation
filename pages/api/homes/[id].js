d935a12a7f775d6ca3fc011ad27082276afe8889
Mali Matiso <s220651337@mandela.ac.za>
2023-01-24 23:12:58 +0200

    API endpoint to update homes

---
+++ pages/api/homes/[id].js
@@ -0,0 +1,39 @@
+import {getSession} from "next-auth/react";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export default async function handler(req, res) {
+    const session = await getSession({ req });
+    if (!session) {
+        return res.status(401).json({ message: 'Unauthorized.' });
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { email: session.user.email },
+        select: { listedHomes: true },
+    });
+
+    const { id } = req.query;
+    if (!user?.listedHomes?.find(home => home.id === id)) {
+        return res.status(401).json({ message: 'Unauthorized.' });
+    }
+
+    if (req.method === 'PATCH') {
+        try {
+            const home = await prisma.home.update({
+                where: { id },
+                data: req.body,
+            });
+            res.status(200).json(home);
+        } catch (e) {
+            res.status(500).json({ message: 'Something went wrong' });
+        }
+    }
+    // HTTP method not supported!
+    else {
+        res.setHeader('Allow', ['PATCH']);
+        res
+            .status(405)
+            .json({ message: `HTTP method ${req.method} is not supported.` });
+    }
+}

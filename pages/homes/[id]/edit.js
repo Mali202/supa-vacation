b331a52bcbd8c231583b2d719a063b4754c063a2
Mali Matiso <s220651337@mandela.ac.za>
2023-01-25 00:07:43 +0200

    Secure page and retrieve home by id parameter

--- pages/homes/[id]/edit.js
+++ pages/homes/[id]/edit.js
@@ -1,5 +1,39 @@
 import Layout from '@/components/Layout';
 import ListingForm from '@/components/ListingForm';
+import { getSession } from 'next-auth/react';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+
+    const redirect = {
+        redirect: {
+            destination: '/',
+            permanent: false,
+        },
+    };
+
+    if (!session) {
+        return redirect;
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { email: session.user.email },
+        select: { listedHomes: true },
+    });
+
+    const id = context.params.id;
+    const home = user?.listedHomes?.find(home => home.id === id);
+    if (!home) {
+        return redirect;
+    }
+
+    return {
+        props: JSON.parse(JSON.stringify(home)),
+    };
+}
 
 const Edit = () => {
     return (

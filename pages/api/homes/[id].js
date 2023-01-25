d49fc8921676c30c48894cc45de6f21d005009d5
Mali Matiso <s220651337@mandela.ac.za>
2023-01-25 23:39:43 +0200

    Delete image from storage when home is deleted

--- pages/api/homes/[id].js
+++ pages/api/homes/[id].js
@@ -34,6 +34,12 @@ export default async function handler(req, res) {
             const home = await prisma.home.delete({
                 where: { id },
             });
+
+            if (home.image) {
+                const path = home.image.split(`${process.env.SUPABASE_BUCKET}/`)?.[1];
+                await supabase.storage.from(process.env.SUPABASE_BUCKET).remove([path]);
+            }
+
             res.status(200).json(home);
         } catch (e) {
             res.status(500).json({ message: 'Something went wrong' });

87de1ce3c394d6f3b4e4ef722f01ff844ab21104
Mali Matiso <s220651337@mandela.ac.za>
2023-01-28 22:30:57 +0200

    API endpoint to retrieve owner of home

---
+++ pages/api/homes/[id]/owner.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            const { id } = req.query;
+            const { owner } = await prisma.home.findUnique({
+                where: { id },
+                select: { owner: true },
+            });
+            res.status(200).json(owner);
+        } catch (e) {
+            res.status(500).json({ message: 'Something went wrong' });
+        }
+    }
+    // HTTP method not supported!
+    else {
+        res.setHeader('Allow', ['GET']);
+        res
+            .status(405)
+            .json({ message: `HTTP method ${req.method} is not supported.` });
+    }
+}

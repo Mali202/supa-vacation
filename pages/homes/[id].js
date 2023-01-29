2735c27aa9c713fef1b6aa95b464946e79728e00
Mali Matiso <s220651337@mandela.ac.za>
2023-01-29 14:53:33 +0200

    Show edit and delete buttons if user is owner of home

--- pages/homes/[id].js
+++ pages/homes/[id].js
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import Layout from '@/components/Layout';
 import { PrismaClient } from '@prisma/client';
 import { useRouter } from 'next/router';
+import {useSession} from "next-auth/react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 // Instantiate Prisma Client
 const prisma = new PrismaClient();
@@ -41,6 +44,23 @@ export async function getStaticProps({ params }) {
 }
 
 const ListedHome = (home = null) => {
+    const { data: session } = useSession();
+    const [isOwner, setIsOwner] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            if (session?.user) {
+                try {
+                    const owner = await axios.get(`/api/homes/${home.id}/owner`);
+                    setIsOwner(owner?.id === session.user.id);
+                } catch (e) {
+                    setIsOwner(false);
+                }
+            }
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [session?.user]);
+
     const router = useRouter();
 
     if (router.isFallback) {
@@ -69,6 +89,26 @@ const ListedHome = (home = null) => {
                             </li>
                         </ol>
                     </div>
+
+                    {isOwner ? (
+                        <div className="flex items-center space-x-2">
+                            <button
+                                type="button"
+                                onClick={() => router.push(`/homes/${home.id}/edit`)}
+                                className="px-4 py-1 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition rounded-md disabled:text-gray-800 disabled:bg-transparent disabled:opacity-50 disabled:cursor-not-allowed"
+                            >
+                                Edit
+                            </button>
+
+                            <button
+                                type="button"
+                                onClick={() => null}
+                                className="rounded-md border border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white focus:outline-none transition disabled:bg-rose-500 disabled:text-white disabled:opacity-50 disabled:cursor-not-allowed px-4 py-1"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    ) : null}
                 </div>
 
                 <div className="mt-6 relative aspect-w-16 aspect-h-9 bg-gray-200 rounded-lg shadow-md overflow-hidden">
